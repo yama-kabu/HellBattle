9516fa1975db39cec20930b83acd82d56bcef6a1
RMN0605 <20c1020027dr@edu.tech.ac.jp>
2021-06-03 17:19:22 +0900

    6/3 17:19
    モック版

---
+++ HBB_DR/Assets/Player/Player_Manager/Player_Manager_L1.cs
@@ -0,0 +1,40 @@
+//ル
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Manager_L1 : MonoBehaviour
+{
+    //Rigidbody2D コンポーネントを格納する変数
+    private Rigidbody2D Player_L;
+
+    private Rigidbody2D Reflect;
+//--------------------------------------------------------------------------------------
+
+    // ゲームのスタート時の処理
+    void Start()
+    {
+        //Rigidbody2D　コンポーネントを取得して変数　Player_L　に格納
+        Player_L = GetComponent<Rigidbody2D>();
+    }
+
+//--------------------------------------------------------------------------------------
+
+    // 繰り返す処理
+    void Update()
+    {
+        Shot_Manager1 s_Manager1 = GetComponent<Shot_Manager1>();
+        //s_Manager1.Shot1();//Way
+        s_Manager1.Shot2();//spiral
+        s_Manager1.Shot3();//Homing
+        s_Manager1.Shot4();//Reflect
+        s_Manager1.Shot5();//Barrage
+
+
+        //s_Manager.Shot6();//Random_Homing
+        ////////////////////↓Shot7からお願いします
+    }
+
+//--------------------------------------------------------------------------------------
+
+}
