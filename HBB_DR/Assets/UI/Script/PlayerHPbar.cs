6262b8244512e2c62c703e9ff07d318a98b034d8
kumachan390 <kuma.0209@icloud.com>
2021-06-12 17:35:50 +0900

    タイトル、ゲームシーンの編集
    タイトルの画像差し替え
    体力バーの増減スクリプト

--- HBB_DR/Assets/UI/Script/PlayerHPbar.cs
+++ HBB_DR/Assets/UI/Script/PlayerHPbar.cs
@@ -5,30 +5,43 @@ using UnityEngine.UI;
 
 public class PlayerHPbar : MonoBehaviour
 {
-    Slider HPSlider;
-    // Start is called before the first frame update
-    void Start()
+    public Image HPGauge;
+
+    private void Update()
     {
-        HPSlider = GetComponent<Slider>();
+        //プレイヤーの取得
+        var player = Player_Manager_R.p_instance;
 
-        float MaxHP = 200f;
-        float NowHP = 200f;
-
-        HPSlider.maxValue = MaxHP;//スライダーの最大値の設定
-        HPSlider.value = NowHP;//スライダーの現在値の設定
+        //HPのゲージの表示を更新する
+        var hp = player.m_Player_HP;
+        var hpMax = player.m_Player_MAXHP;
+        HPGauge.fillAmount = (float)hp / hpMax;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))//左クリックで体力バーを減らす
-        {
-            HPSlider.value -= 10f;
-        }
-
-        if(Input.GetKey(KeyCode.K))
-        {
-            HPSlider.value += 0.1f;
-        }
-    }
 }
+//Slider HPSlider;
+//// Start is called before the first frame update
+//void Start()
+//{
+//    HPSlider = GetComponent<Slider>();
+
+//    float MaxHP = 200f;
+//    float NowHP = 200f;
+
+//    HPSlider.maxValue = MaxHP;//スライダーの最大値の設定
+//    HPSlider.value = NowHP;//スライダーの現在値の設定
+//}
+
+//// Update is called once per frame
+//void Update()
+//{
+//    if (Input.GetMouseButtonDown(0))//左クリックで体力バーを減らす
+//    {
+//        HPSlider.value -= 10f;
+//    }
+
+//    if (Input.GetKey(KeyCode.K))
+//    {
+//        HPSlider.value += 0.1f;
+//    }
+//}
