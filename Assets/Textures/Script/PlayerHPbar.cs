74e1076007f061b048802261346c7f7e59fa8d9e
zerovanisu <67454773+zerovanisu@users.noreply.github.com>
2021-07-01 13:44:27 +0900

    HPbar+イベントを統合

---
+++ Assets/Textures/Script/PlayerHPbar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPbar : MonoBehaviour
+{
+    public GameObject Player;
+
+    public Slider HPGauge;
+
+    private float PlayerHP,PlayerHPMax,CurrentHP;
+
+    void Start()
+    {
+        PlayerHPMax = Player.GetComponent<Player_Manager_R>().Player_MAXHP;
+
+        HPGauge.maxValue = PlayerHPMax;
+    }
+
+    private void Update()
+    {
+        PlayerHP = Player.GetComponent<Player_Manager_R>().Player_HP;
+
+        HPGauge.value = PlayerHP;
+    }
+}
