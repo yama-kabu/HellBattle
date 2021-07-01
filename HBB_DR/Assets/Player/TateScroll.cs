c2ccf58585b71544cc4d0b31769889cbd06b2a91
RMN0605 <20c1020027dr@edu.tech.ac.jp>
2021-07-01 18:38:29 +0900

    7/1
    field背景変更
    stage背景の移動追加

--- HBB_DR/Assets/Player/TateScroll.cs
+++ HBB_DR/Assets/Player/TateScroll.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TateScroll : MonoBehaviour
 {
     //スクロールスピード
-    [SerializeField] float speed = 1;
+    [SerializeField] float speed = 100;
 
     void Update()
     {
         //下方向にスクロール
         transform.position -= new Vector3(0, Time.deltaTime * speed);
 
-        //Yが-2089まで来れば、4196まで移動する
-        if (transform.position.y <= -2089f)
+        //Yが-1699まで来れば、1685まで移動する
+        if (transform.position.y <= -1699f)
         {
-            transform.position = new Vector2(0, 4196);
+            transform.position = new Vector2(0, 1685f);
         }
+
     }
 }
