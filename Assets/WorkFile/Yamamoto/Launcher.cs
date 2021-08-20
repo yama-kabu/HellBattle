878335015382a954bef3fb02207a43954c0d6a2d
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2021-08-20 10:17:52 +0900

    とりあえず

--- Assets/WorkFile/Yamamoto/Launcher.cs
+++ Assets/WorkFile/Yamamoto/Launcher.cs
@@ -1,138 +1,138 @@
-//using UnityEngine;
-//using Photon.Pun;
-//using Photon.Realtime;
-//using UnityEngine.SceneManagement;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
-//public class Launcher : MonoBehaviourPunCallbacks
-//{
-//    #region Private Serializable Fields
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Private Serializable Fields
 
-//    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
-//    [SerializeField]
-//    private byte maxPlayersPerRoom = 4;
-//    bool isConnecting;
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+    bool isConnecting;
 
-//    #endregion
-
-
-//    #region Private Fields
-//    //[Tooltip("The Ui Panel to let the user enter name, connect and play")]
-//    //[SerializeField]
-//    //private GameObject controlPanel;
-//    //[Tooltip("The UI Label to inform the user that the connection is in progress")]
-//    //[SerializeField]
-//    //private GameObject progressLabel;
-
-//    string gameVersion = "1";
+    #endregion
 
 
-//    #endregion
+    #region Private Fields
+    //[Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    //[SerializeField]
+    //private GameObject controlPanel;
+    //[Tooltip("The UI Label to inform the user that the connection is in progress")]
+    //[SerializeField]
+    //private GameObject progressLabel;
+
+    string gameVersion = "1";
 
 
-//    #region MonoBehaviour CallBacks
+    #endregion
 
 
-//    void Awake()
-//    {
-//        PhotonNetwork.AutomaticallySyncScene = true;
-//    }
+    #region MonoBehaviour CallBacks
 
 
-//    void Start()
-//    {
-//        //progressLabel.SetActive(false);
-//        //controlPanel.SetActive(true);
-//    }
-
-//    void Update()
-//    {
-
-//        if (Input.GetKey(KeyCode.Z))
-//        {
-//            if (PhotonNetwork.IsConnected)
-//            {
-//                PhotonNetwork.JoinRandomRoom();
-//            }
-//            else
-//            {
-//                isConnecting = PhotonNetwork.ConnectUsingSettings();
-//                PhotonNetwork.GameVersion = gameVersion;
-//            }
-
-//        }
-//        if (Input.GetKey(KeyCode.X))
-//        {
-//            SceneManager.LoadScene("TitleScene");
-//        }
-//    }
-//    #endregion
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
 
 
-//    #region Public Methods
+    void Start()
+    {
+        //progressLabel.SetActive(false);
+        //controlPanel.SetActive(true);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                isConnecting = PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+    #endregion
 
 
-//    //public void Connect()
-//    //{
-//    //    //progressLabel.SetActive(true);
-//    //    //controlPanel.SetActive(false);
-//    //    if (PhotonNetwork.IsConnected)
-//    //    {
-//    //        PhotonNetwork.JoinRandomRoom();
-//    //    }
-//    //    else
-//    //    {
-//    //        isConnecting = PhotonNetwork.ConnectUsingSettings();
-//    //        PhotonNetwork.GameVersion = gameVersion;
-//    //    }
-//    //}
+    #region Public Methods
 
 
-//    #endregion
+    public void Connect()
+    {
+        //progressLabel.SetActive(true);
+        //controlPanel.SetActive(false);
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
 
-//    #region MonoBehaviourPunCallbacks Callbacks
+
+    #endregion
+
+    #region MonoBehaviourPunCallbacks Callbacks
 
 
-//    public override void OnConnectedToMaster()
-//    {
-//        if (isConnecting)
-//        {
-//            PhotonNetwork.JoinRandomRoom();
-//            isConnecting = false;
-//            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
-//        }
-//    }
+    public override void OnConnectedToMaster()
+    {
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        }
+    }
 
 
-//    public override void OnDisconnected(DisconnectCause cause)
-//    {
-//        //progressLabel.SetActive(false);
-//        //controlPanel.SetActive(true);
-//        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
-//    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        //progressLabel.SetActive(false);
+        //controlPanel.SetActive(true);
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
 
-//    public override void OnJoinRandomFailed(short returnCode, string message)
-//    {
-//        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
 
-//        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
-//    }
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
 
-//    public override void OnJoinedRoom()
-//    {
-//        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
-//        {
-//            Debug.Log("We load the 'Taiki' ");
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the 'Taiki' ");
 
-//            PhotonNetwork.LoadLevel("Taiki");
-//        }
-//        if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
-//        {
-//            Debug.Log("We load the 'ButtleScene' ");
+            PhotonNetwork.LoadLevel("Taiki");
+        }
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            Debug.Log("We load the 'Room for 2' ");
 
-//            PhotonNetwork.LoadLevel("ButtleScene");
-//        }
-//        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
-//    }
+            PhotonNetwork.LoadLevel("Room for 2");
+        }
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+    }
 
-//    #endregion
-//}
+    #endregion
+}
--- Assets/WorkFile/Yamamoto/Manager.cs
+++ Assets/WorkFile/Yamamoto/Manager.cs
@@ -1,43 +1,43 @@
-//using UnityEngine;
-//using UnityEngine.SceneManagement;
-//using Photon.Pun;
-//using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
 
-//public class Manager : MonoBehaviourPunCallbacks
-//{
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        //ルーム内のIDと名前を表示
-//        foreach (var player in PhotonNetwork.PlayerList)
-//        {
-//            Debug.Log($"{player.NickName}({player.ActorNumber})");
-//        }
-//    }
-//    void LoadArena()
-//    {
-//        if (!PhotonNetwork.IsMasterClient)
-//        {
-//            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
-//        }
-//        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
-//        PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);//条件に合ったルームに変更する
-//    }
-//    public override void OnPlayerEnteredRoom(Player other)
-//    {
-//        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName);
+public class Manager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ルーム内のIDと名前を表示
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            Debug.Log($"{player.NickName}({player.ActorNumber})");
+        }
+    }
+    void LoadArena()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+        PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);//条件に合ったルームに変更する
+    }
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName);
 
-//        if (PhotonNetwork.IsMasterClient)
-//        {
-//            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
-//            LoadArena();
-//        }
-//    }
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+            LoadArena();
+        }
+    }
 
-//    public override void OnPlayerLeftRoom(Player other)
-//    {
-//        Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
-//        LoadArena();
-//    }
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient);
+        LoadArena();
+    }
 
-//}
+}
