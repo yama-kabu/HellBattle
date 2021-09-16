ebc0a50e1ba24d48c42dcc0aac6fd25c8e6a6c70
zerovanisu <67454773+zerovanisu@users.noreply.github.com>
2021-09-16 22:14:18 +0900

    Revert "Effect調整09/16"

--- Assets/Event/Effect/PlayerEffect.cs
+++ Assets/Event/Effect/PlayerEffect.cs
@@ -5,47 +5,14 @@ using UnityEngine;
 public class PlayerEffect : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private float ScaleMax;
-
-    [SerializeField]
-    private float ScaleLeast;
-
-    [SerializeField]
-    private float ChangeSpeed;
-
-    Vector3 E_Scale;
-    Vector3 ChangeScale;
-    bool ScaleSwitch;
-
     void Start()
     {
-        E_Scale = transform.localScale;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(E_Scale.x >= ScaleMax)
-        {
-            ScaleSwitch = true;
-        }
-        else if(E_Scale.x <= ScaleLeast)
-        {
-            ScaleSwitch = false;
-        }
         
-        if(ScaleSwitch == true)
-        {
-            E_Scale.x = E_Scale.y = E_Scale.z -= ChangeSpeed;
-            gameObject.transform.localScale = E_Scale;
-            //Debug.Log("¬‚³‚­‚µ‚Ä‚é");
-        }
-        else
-        {
-            E_Scale.x = E_Scale.y = E_Scale.z += ChangeSpeed;
-            gameObject.transform.localScale = E_Scale;
-            //Debug.Log("‘å‚«‚­‚µ‚Ä‚é");
-        }
     }
 }
